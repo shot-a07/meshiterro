c34bed8edd5e88cfece58845e9668a11be9424a9
shot-a07 <92033602+shot-a07@users.noreply.github.com>
2021-11-17 14:45:02 +0000

    Update meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1e84fef19833032ef31f252411ec747ef680e70b62738cd1191fa73ec7b30d40aa6d5efa24197814adb1de10b8b9ad3e3cec96ab9e08622dcba37df4fd7b4727'
